a972247a7f764fea61c9d17b80151e4d5c921ccf
Ellie Richardson-Jones <elliebethjones@gmail.com>
2020-01-22 15:53:05 +0000

    Docking a bike to the docking station

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -11,3 +11,21 @@ describe DockingStation do
     expect(bike).to be_working
   end
 end
+
+describe DockingStation do 
+  it 'docks bike in docking station' do
+    docking_station = subject.dock(:bike)
+    expect(docking_station).to respond_to 
+  end
+end
+
+=begin
+describe DockingStation do
+  it 'dock method stores bike atribute in dockingstation class object' do
+    station = DockingStation.new
+    bike = Bike.new 
+    station.dock(bike)
+    expect(station[:bike]).to eq(bike)
+  end
+end
+=end
